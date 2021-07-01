e259431cc994dd39e4523f51042e082eb1068917
JoaoCostaIFG <joaocosta.work@posteo.net>
2021-07-01 04:07:03 +0100

    WIP: big rework of the website.

---
+++ database/db.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * Singleton class representating the database connection
+ */
+class Database
+{
+  private static $instance = NULL;
+  private $db = NULL;
+
+  /**
+   * Private constructor. Creates db connection.
+   */
+  private function __construct() {
+    $this->db = new PDO('sqlite:db.db');
+    $this->db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    if (NULL == $this->db) 
+      throw new Exception("Failed to open database.");
+  }
+
+  /**
+   * @return database connection
+   */
+  public function db() {
+    return $this->db;
+  }
+  
+  /**
+   * @return singleton instance (instantiated if needed)
+   */
+  static function instance() {
+    if (self::$instance == NULL) {
+      self::$instance = new Database();
+    }
+
+    return self::$instance;
+  }
+}
