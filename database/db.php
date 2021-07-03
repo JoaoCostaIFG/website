207ef59171462e8c3de0d23f69d57b69d301e1cc
JoaoCostaIFG <joaocosta.work@posteo.net>
2021-07-03 03:39:09 +0100

    Update: moved pragas to database php script; Add: WAL to db.

--- database/db.php
+++ database/db.php
@@ -14,8 +14,13 @@ class Database
   private function __construct()
   {
     $this->db = new PDO('sqlite:database/db.db');
+
     $this->db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $this->db->query('PRAGMA foreign_keys=ON');
+    $this->db->query('PRAGMA journal_mode=WAL');
+
     if (NULL == $this->db)
       throw new Exception("Failed to open database.");
   }
