d978a86a5d18e688ffe6ed0676703e137cda40b5
JoaoCostaIFG <joaocosta.work@posteo.net>
2021-07-06 23:41:59 +0100

    Update: Home blog list is now dynamic.

--- App/Controllers/HomeController.php
+++ App/Controllers/HomeController.php
@@ -2,11 +2,13 @@
 
 namespace Controllers;
 
+use Models\BlogModel;
+
 class HomeController
 {
   public static function show()
   {
-    view('home.php');
+    view_args('home.php', array('bs' => BlogModel::some(4)));
   }
 
   public static function redirect()
