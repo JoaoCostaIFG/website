d4867f262059ca3cd7914ccf66adcd8107e1815d
JoaoCostaIFG <joaocosta.work@posteo.net>
2021-07-08 04:05:41 +0100

    Add: project entry edit. Update: nginx gzip settings.

--- resources/views/partials/projs/entry.php
+++ resources/views/partials/projs/entry.php
@@ -2,6 +2,8 @@
   <a href="<?= $args['p']->getUrl(); ?>"><?= $args['p']->getTitle(); ?></a>
 </h3>
 
+<a href="<?= route_args('proj_edit_route', array('id' =>  $args['p']->getId())); ?>">Edit</a>
+
 <img alt="<?= $args['p']->getTitle() ?>" src="<?= img('projects/' . $args['p']->getImg()); ?>" width="128">
 <p>
   <?= Parsedown::instance()->line($args['p']->getDescription()); ?>
