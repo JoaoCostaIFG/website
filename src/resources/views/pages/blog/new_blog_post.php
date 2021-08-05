3a147d37e3956eecf7ccbe62edc38894f7c69a5e
JoaoCostaIFG <joaocosta.work@posteo.net>
2021-08-05 03:21:56 +0100

    Update: form appearance.

--- src/resources/views/pages/blog/new_blog_post.php
+++ src/resources/views/pages/blog/new_blog_post.php
@@ -2,21 +2,30 @@
 
 <h2>New blog post</h2>
 
-<form method="POST" action="<?=route('blog_insert_action');?>">
-  <?=partial('csrf.php');?>
+<form method="POST" action="<?= route('blog_insert_action'); ?>">
+  <?= partial('csrf.php'); ?>
 
-  <label for="title"><b>Title *</b></label>
-  <input type="text" id="title" name="title" placeholder="Post title..." required autofocus><br>
-  <label for="intro"><b>Intro</b></label>
-  <textarea id="intro" name="intro" rows="3" placeholder="Post summary/introduction..."></textarea><br>
-  <label for="content"><b>Content *</b></label>
-  <textarea id="content" name="content" rows="20" placeholder="Post body..." required></textarea><br>
-  <label for="date"><b>Date</b></label>
-  <input type="date" id="date" name="date"><br>
-  <label for="visibility"><b>Publicly visible</b></label>
-  <input type="checkbox" id="visibility" name="visibility"><br>
+  <div class="row">
+    <div class="seven columns">
+      <label for="title">Title *</label>
+      <input class="u-full-width" type="text" id="title" name="title" placeholder="Post title..." required autofocus>
+    </div>
+    <div class="five columns">
+      <label for="date">Date</label>
+      <input type="date" id="date" name="date">
+    </div>
+  </div>
 
-  <input type="submit" value="Submit">
+  <label for="intro">Intro</label>
+  <textarea class="u-full-width" id="intro" name="intro" rows="3" placeholder="Post summary/introduction..."></textarea>
+  <label for="content">Content *</label>
+  <textarea class="u-full-width" id="content" name="content" rows="30" placeholder="Post body..." required></textarea>
+
+  <label class="u-pull-right">
+    <input type="checkbox" id="visibility" name="visibility">
+    <span>Publicly visible</span>
+  </label>
+  <input class="button-primary" type="submit" value="Submit">
 </form>
 
 <?php layout_footer(); ?>
