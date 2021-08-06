bfe7b23abfc620a8e79d04da9ce87b6adab836a0
JoaoCostaIFG <joaocosta.work@posteo.net>
2021-08-06 23:31:40 +0100

    Update: about me page.

--- src/resources/views/pages/about.php
+++ src/resources/views/pages/about.php
@@ -1,53 +1,44 @@
 <?php layout_header('About'); ?>
 
-<h2>Hello c:</h2>
-<p>
-  I'm a computer engineering student at the
-  <a href="https://fe.up.pt"
-    >Faculty of Engineering of the University of Porto</a
-  >
-  and this is my personal website/blog.<br />
-  I created this website to share/talk about things that I feel like talking
-  about so this isn't strictly a technology/informatics blog.<br />
-</p>
-<p>
-  The programming languages I'm most comfortable with are
-  <b>Bash/POSIX shell</b> (if we can call shell scripts programming languages),
-  <b>C</b>, <b>C++</b>, <b>java</b> and <b>Python3</b>, but I also have some
-  experience with <b>ARMv7/8 assembly</b>, <b>C#</b>, <b>Pascal</b>,
-  <b>VB.Net</b> and <b>javascript</b>.
-</p>
-<p>
-  In my free time, I enjoy playing games, reading, listening to music, and
-  working on personal projects (which usually revolve around customizing my
-  Linux machines) <i>(I use Arch BTW)</i>.<br />
-</p>
-<p>
-  I'm a firm supporter of the sovereignty of people's data (<b>privacy</b>),
-  <a href="https://www.fsf.org">free software</a> (or at least open-source
-  software) and the <a href="https://repair.eu">right to repair</a>.
-</p>
-<p>
-  Generally speaking, I don't like social media so I try to stay as far away
-  from it as possible. This is mostly due to privacy concerns but also because I
-  believe it is harmful to the human psyche. I liked this
-  <a href="https://www.youtube.com/watch?v=YjbyDU0WzYI">video</a> by
-  <a href="https://lukesmith.xyz">Luke Smith</a> about the topic.
-</p>
-<p>
-  I don't want to share my face on the website (at least for the time being) so
-  I'll use the profile picture I use in most places, which you can see below.
-  This drawing was based on the Desert Skate "Floof" decoration from the game
-  <a href="https://www.warframe.com">Warframe</a> and it was drawn by someone
-  very dear to me who goes by the pseudonym <b>o̶̅̑n̵̓͒i̷̋͆</b> (use of the image by
-  other entities is <b>not</b> permitted).
-  <center>
-    <img
-      src="/static/img/logo.png"
-      alt="Space Dragon"
-      style="max-width: 30%; height: auto;"
-    />
-  </center>
-</p>
+<h2>Hello c|:D</h2>
+
+<div class="row">
+  <div class="four columns">
+    <img src="<?= img('pfp.jpg') ?>" alt="Me" class="u-full-width">
+    <p>
+      (This is the profile picture I usually use online. It was generated using
+      my <a href="https://gitlab.com/JoaoCostaIFG/allrgb">AllRGB program</a>.)
+    </p>
+  </div>
+  <div class="eight columns">
+    <p>
+      I'm a twenty-something year old computer engineering student at the
+      <a href="https://fe.up.pt">Faculty of Engineering of the University of Porto</a>
+      and this is my personal website.<br>
+      I started this website so I could learn more about web development and have
+      a place to share my experiences, mostly relating to programming although not
+      necessarly.
+    </p>
+    <p>
+      Over the years, I've dealt with numerous programming languages, but I am
+      the most confortable with the following: <b>Bash/POSIX shell</b>,
+      <b>C</b>, <b>C++</b>, <b>Java</b>, <b>Python3</b>, and <b>Zig</b>.
+    </p>
+    <p>
+      In my free time, I enjoy playing games, reading, listening to music, and
+      working on personal projects, which usually revolve around customizing my
+      Linux machines <i>(I use Arch BTW)</i>.<br>
+    </p>
+    <p>
+      I'm a supporter of the sovereignty of people's data (<b>privacy</b>),
+      <a href="https://www.fsf.org">free software</a> (or at least open-source
+      software) and the <a href="https://repair.eu">right to repair</a>.<br>
+      Generally speaking, I don't like social media so I try to stay as far away
+      from it as possible. This is mostly due to privacy concerns but also because I
+      believe it is harmful to the human psyche. You can contact me using the following
+      email address: <b>joaocosta.work@posteo.net</b>.
+    </p>
+  </div>
+</div>
 
 <?php layout_footer(); ?>
