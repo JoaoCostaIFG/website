a747bc9b4f895144de463c2671a9ca2966a95b6a
JoaoCostaIFG <joaocosta.work@posteo.net>
2021-08-07 00:12:41 +0100

    Update: new proj form

--- src/resources/views/pages/projs/new_proj.php
+++ src/resources/views/pages/projs/new_proj.php
@@ -2,20 +2,26 @@
 
 <h2>New project</h2>
 
-<form method="POST" action="<?=route('proj_insert_action');?>" enctype="multipart/form-data">
-  <?=partial('csrf.php');?>
+<form method="POST" action="<?= route('proj_insert_action'); ?>" enctype="multipart/form-data">
+  <?= partial('csrf.php'); ?>
 
-  <label for="title"><b>Title *</b></label>
-  <input type="text" id="title" name="title" placeholder="Proj title..." required autofocus><br>
-  <label for="description"><b>Description</b></label>
-  <textarea id="description" name="description" rows="20" placeholder="Proj description..."></textarea><br>
-  <label for="url"><b>Url *</b></label>
-  <input type="url" id="url" name="url" placeholder="Proj url..." required><br>
-  <label for="img"><b>Img *</b></label>
-  <input type="file" id="img" name="img" required><br>
+  <div class="row">
+    <div class="six columns">
+      <label for="title">Title *</label>
+      <input class="u-full-width" type="text" id="title" name="title" placeholder="Proj title..." required autofocus>
+    </div>
+    <div class="six columns">
+      <label for="url">Url *</label>
+      <input class="u-full-width" type="url" id="url" name="url" placeholder="Proj url..." required>
+    </div>
+  </div>
 
-  <input type="submit" value="Submit">
+  <label for="description">Description</label>
+  <textarea class="u-full-width" id="description" name="description" rows="20" placeholder="Proj description..."></textarea>
+  <label for="img">Img *</label>
+  <input class="u-full-width" type="file" id="img" name="img" required>
+
+  <input class="button-primary" type="submit" value="Submit">
 </form>
 
 <?php layout_footer(); ?>
-
