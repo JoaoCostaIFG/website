0937195ae9d3c650a6a32f1adc2c2a19c44089cc
JoaoCostaIFG <joaocosta.work@posteo.net>
2021-08-11 17:28:44 +0100

    Update: pretty edit button on projects grid.

--- src/resources/views/partials/projs/entry.php
+++ src/resources/views/partials/projs/entry.php
@@ -3,13 +3,13 @@
     <a href="<?= $args['p']->getUrl(); ?>"><?= $args['p']->getTitle(); ?></a>
   </h3>
 
-  <?php if (is_auth()) { ?>
-    <a href="<?= route_args('proj_edit_route', array('id' =>  $args['p']->getId())); ?>">Edit</a>
-  <?php } ?>
-
   <img alt="<?= $args['p']->getTitle() ?>" src="<?= img('projects/' . $args['p']->getImg()); ?>">
   <hr>
   <p>
     <?= Parsedown::instance()->line($args['p']->getDescription()); ?>
   </p>
+
+  <?php if (is_auth()) { ?>
+    <a class="button proj-edit-btn" href="<?= route_args('proj_edit_route', array('id' =>  $args['p']->getId())); ?>">Edit</a>
+  <?php } ?>
 </article>
