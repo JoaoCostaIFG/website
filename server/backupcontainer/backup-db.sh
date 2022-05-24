52e1a0c751f9df3157099a7d7f359d4d94b80b1a
JoaoCostaIFG <joaocosta.work@posteo.net>
2022-05-24 21:06:46 +0100

    Add: backup container.

---
+++ server/backupcontainer/backup-db.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+docker run --rm -v site_app_data:/site_app_data -v site_tor_data:/site_tor_data -v "$(pwd)":/backup backupcontainer
---
+++ server/backupcontainer/entry.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -e
+
+# app data
+rsync -a -q '/site_app_data' '/backup' --exclude '/site_app_data/main/database/db.db'
+sqlite3 '/site_app_data/main/database/db.db' '/backup/site_app_data/main/database/db.db'
+tar -czf '/backup/site_app_data.tar.gz' 'backup/site_app_data'
+rm -rf '/backup/site_app_data'
+
+# tor data
+tar -czf '/backup/site_tor_data.tar.gz' 'site_tor_data'
