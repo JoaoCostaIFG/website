58fd24c207b3405f9fe74cb8aaad8896691085b7
JoaoCostaIFG <joaocosta.work@posteo.net>
2022-05-24 21:13:51 +0100

    I'm dumb..

--- server/backupcontainer/entry.sh
+++ server/backupcontainer/entry.sh
@@ -3,8 +3,8 @@
 set -e
 
 # app data
-rsync -a -q '/site_app_data' '/backup' --exclude '.backup /site_app_data/main/database/db.db'
-sqlite3 '/site_app_data/main/database/db.db' '/backup/site_app_data/main/database/db.db'
+rsync -a -q '/site_app_data' '/backup' --exclude '/site_app_data/main/database/db.db'
+sqlite3 '/site_app_data/main/database/db.db' '.backup /backup/site_app_data/main/database/db.db'
 tar -czf '/backup/site_app_data.tar.gz' 'backup/site_app_data'
 rm -rf '/backup/site_app_data'
 
