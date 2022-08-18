4c54bbbbf4709e0c502c14da52799b7dd894254e
JoaoCostaIFG <joaocosta.work@posteo.net>
2022-08-19 00:56:29 +0100

    Update: style 'more about me' button

--- site/src/resources/views/pages/home.php
+++ site/src/resources/views/pages/home.php
@@ -5,7 +5,7 @@
 <div class="grid grid-cols-12 gap-x-4 gap-y-4">
   <section class="col-span-12 md:col-span-7">
     <h2>Recent posts</h2>
-    <div class="grid grid-cols-1 gap-y-1  mb-4">
+    <div class="grid grid-cols-1 gap-y-1 mb-4">
       <?php
       foreach ($args['bs'] as $b) {
         partial_args('blog/homeentry.php', array('b' => $b));
@@ -22,16 +22,17 @@
 
   <section class="col-span-12 md:col-span-5">
     <h2>About</h2>
-    <p>
+    <p class="mb-4">
       Hey! My name is João Costa and this is my personal corner of the internet.
-    </p>
-    <p>
       I'm interested in computer science and electronics, and I enjoy implementing my own solutions to problems/needs.
-    </p>
-    <p>
       This page's main focus is for me to share some ideas/processes behind projects that I've worked on.
     </p>
-    <a href="<?= route('about_route'); ?>">(see more about me)</a>
+    <div class="text-right">
+      <a class="btn bg-primary-500 hover:bg-primary-400 dark:bg-primary-600 dark:hover:bg-primary-700"
+          href="<?= route('about_route'); ?>">
+        More About Me
+      </a>
+    </div>
   </section>
 
   <section class="col-span-12">
