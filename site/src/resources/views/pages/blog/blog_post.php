6546aacbb263eb8275d2f8fef0bb37b918316cde
JoaoCostaIFG <joaocosta.work@posteo.net>
2022-08-19 22:12:36 +0100

    Update: center img, margin table, code small text

--- site/src/resources/views/pages/blog/blog_post.php
+++ site/src/resources/views/pages/blog/blog_post.php
@@ -7,28 +7,30 @@ if ($has_code_blocks) layout_header_args(array('title' => $args['b']->getTitle()
 else layout_header($args['b']->getTitle());
 ?>
 
-<article class="prose relative line-numbers match-braces">
-  <?php if (is_auth()) { ?>
-    <a class="absolute top-0 right-0 z-10 icon-btn btn-edit" title="Edit post <?= $args['b']->getId() ?>" href="<?= route_args('blog_edit_route', array('id' => $args['b']->getId())); ?>">
-      <i class="fa-solid fa-pen-to-square"></i>
-    </a>
-  <?php } ?>
+<div class="w-full">
+  <article class="m-auto relative prose blog line-numbers match-braces">
+    <?php if (is_auth()) { ?>
+      <a class="absolute top-0 right-0 z-10 icon-btn btn-edit" title="Edit post <?= $args['b']->getId() ?>" href="<?= route_args('blog_edit_route', array('id' => $args['b']->getId())); ?>">
+        <i class="fa-solid fa-pen-to-square"></i>
+      </a>
+    <?php } ?>
 
-  <div class="mb-4 flex flex-row flex-wrap flex-start items-baseline gap-x-4">
-    <h1 class="inline-block mb-0">
-      <?= Parsedown::instance()->line($args['b']->getTitle()); ?>
-    </h1>
-    <em class="muted">Avg. <?= $args['b']->readingTime(); ?> minute(s) of reading</em>
-  </div>
-
-  <?php if (!is_null($args['b']->getIntro())) { ?>
-    <div class="mx-2 p-2 rounded-md bg-background-300 dark:bg-background-900">
-      <?= Parsedown::instance()->text($args['b']->getIntro()); ?>
+    <div class="mb-4 flex flex-row flex-wrap flex-start items-baseline gap-x-4">
+      <h1 class="inline-block mb-0">
+        <?= Parsedown::instance()->line($args['b']->getTitle()); ?>
+      </h1>
+      <em class="muted">Avg. <?= $args['b']->readingTime(); ?> minute(s) of reading</em>
     </div>
-  <?php } ?>
 
-  <?= Parsedown::instance()->text($args['b']->getContent()); ?>
-</article>
+    <?php if (!is_null($args['b']->getIntro())) { ?>
+      <div class="mx-2 p-2 rounded-md bg-background-300 dark:bg-background-900">
+        <?= Parsedown::instance()->text($args['b']->getIntro()); ?>
+      </div>
+    <?php } ?>
+
+    <?= Parsedown::instance()->text($args['b']->getContent()); ?>
+  </article>
+</div>
 
 <?php
 
