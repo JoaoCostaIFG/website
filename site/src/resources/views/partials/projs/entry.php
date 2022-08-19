497565d46dcfc9a76db692e2e257108fec6d2237
JoaoCostaIFG <joaocosta.work@posteo.net>
2022-08-19 15:32:55 +0100

    Update: proj title clamp.

--- site/src/resources/views/partials/projs/entry.php
+++ site/src/resources/views/partials/projs/entry.php
@@ -1,10 +1,10 @@
-<article class="relative p-3 rounded-lg max-w-xs flex flex-col text-center items-center bg-background-900">
-  <h2>
-    <a href="<?= $args['p']->getUrl(); ?>"><?= $args['p']->getTitle(); ?></a>
+<article class="relative p-3 rounded-lg max-w-xs flex flex-col text-center items-center bg-background-200 hover:bg-background-300 dark:bg-background-900 hover:dark:bg-gray-900">
+  <h2 class="line-clamp-1">
+    <a title="<?= $args['p']->getTitle(); ?>" href="<?= $args['p']->getUrl(); ?>"><?= $args['p']->getTitle(); ?></a>
   </h2>
   <div class="bg-contain bg-no-repeat bg-center w-full h-28" title="<?= $args['p']->getTitle() ?>" style="background-image: url(<?= img('projects/' . $args['p']->getImg()); ?>)">
   </div>
-  <hr class="border-1 w-full my-4">
+  <hr class="border-1 w-full my-4 border-foreground-600 dark:border-foreground-500">
   <p>
     <?= Parsedown::instance()->line($args['p']->getDescription()); ?>
   </p>
