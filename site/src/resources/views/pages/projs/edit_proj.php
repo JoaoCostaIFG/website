45758fcdb9991dbf1c3094c0101dfe107e18f068
JoaoCostaIFG <joaocosta.work@posteo.net>
2022-08-20 16:30:13 +0100

    Add: style edit proj page

--- site/src/resources/views/pages/projs/edit_proj.php
+++ site/src/resources/views/pages/projs/edit_proj.php
@@ -1,29 +1,37 @@
 <?php layout_header('Edit blog post'); ?>
 
-<h2>Edit blog post</h2>
+<h1>Edit blog post</h1>
 
-<form method="POST" action="<?= route('proj_edit_action'); ?>" enctype="multipart/form-data">
-  <?= partial('csrf.php'); ?>
-  <input type="hidden" id="id" name="id" value="<?= $args['p']->getId(); ?>">
-  <input type="hidden" id="old_img" name="old_img" value="<?= $args['p']->getImg(); ?>">
+<div class="w-full">
+  <form class="m-auto max-w-prose" method="POST" action="<?= route('proj_edit_action'); ?>" enctype="multipart/form-data">
+    <?= partial('csrf.php'); ?>
+    <input type="hidden" id="id" name="id" value="<?= $args['p']->getId(); ?>">
+    <input type="hidden" id="old_img" name="old_img" value="<?= $args['p']->getImg(); ?>">
 
-  <div class="row">
-    <div class="six columns">
-      <label for="title">Title *</label>
-      <input class="u-full-width" type="text" id="title" name="title" value="<?= $args['p']->getTitle(); ?>" placeholder="Proj title..." required autofocus>
+    <div>
+      <label class="form-label form-label-required" for="title">Title</label>
+      <input class="form-input" id="title" type="text" name="title" placeholder="Project title..." required autofocus value="<?= $args['p']->getTitle(); ?>">
     </div>
-    <div class="six columns">
-      <label for="url">Url *</label>
-      <input class="u-full-width" type="url" id="url" name="url" value="<?= $args['p']->getUrl(); ?>" placeholder="Proj url..." required>
+
+    <div class="mt-6">
+      <label class="form-label form-label-required" for="url">Url</label>
+      <input class="form-input" id="url" type="url" name="url" placeholder="Project url..." required value="<?= $args['p']->getUrl(); ?>">
     </div>
-  </div>
 
-  <label for="description">Description</label>
-  <textarea class="u-full-width" id="description" name="description" rows="20" placeholder="Proj description..."><?= $args['p']->getDescription(); ?></textarea>
-  <label for="img">Img</label>
-  <input class="u-full-width" type="file" id="img" name="img"><br>
+    <div class="mt-6">
+      <label class="form-label" for="description">Description</label>
+      <textarea class="form-input" id="description" name="description" rows="10" placeholder="Project description..."><?= $args['p']->getDescription(); ?></textarea>
+    </div>
 
-  <input class="button-primary" type="submit" value="Submit">
-</form>
+    <div class="mt-6">
+      <label class="form-label" for="img">Img</label>
+      <input class="form-file" type="file" id="img" name="img">
+    </div>
+
+    <div class="mt-6 text-right">
+      <input class="btn btn-primary" type="submit" value="Submit">
+    </div>
+  </form>
+</div>
 
 <?php layout_footer(); ?>
