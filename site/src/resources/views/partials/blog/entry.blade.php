cd10699f20d5218df60c2486fe1d18d03ee4f556
JoaoCostaIFG <joaocosta.work@posteo.net>
2022-09-02 00:40:20 +0100

    Fix: route to blog

--- site/src/resources/views/partials/blog/entry.blade.php
+++ site/src/resources/views/partials/blog/entry.blade.php
@@ -1,7 +1,7 @@
 <a class="p-2 rounded-md
     bg-background-200 text-foreground-800 dark:bg-background-900 dark:text-foreground-200
     hover:bg-gray-300 dark:hover:bg-gray-900
-    hover:ring-2 hover:ring-primary-500 dark:hover:ring-primary-600" href="{{ route('blog', ['id' => $b->id]) }}">
+    hover:ring-2 hover:ring-primary-500 dark:hover:ring-primary-600" href="{{ route('blog', ['b' => $b]) }}">
   <span class="font-semibold">
     {{ $b->title }}
     @if (!$b->visible)
