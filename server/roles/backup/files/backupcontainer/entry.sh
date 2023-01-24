22a30f13308f4ae0ae5962b5f3fdff78e5eb9df4
JoaoCostaIFG <joaocosta.work@posteo.net>
2023-01-24 01:42:28 +0000

    Backup stuff

--- server/roles/backup/files/backupcontainer/entry.sh
+++ server/roles/backup/files/backupcontainer/entry.sh
@@ -5,8 +5,6 @@ set -e
 # app data
 rsync -a -q '/site_app_data' '/backup' --exclude '/site_app_data/main/database/db.db'
 sqlite3 '/site_app_data/main/database/db.db' '.backup /backup/site_app_data/main/database/db.db'
-tar -czf '/backup/site_app_data.tar.gz' 'backup/site_app_data'
-rm -rf '/backup/site_app_data'
 
 # tor data
-tar -czf '/backup/site_tor_data.tar.gz' 'site_tor_data'
+rsync -a -q '/site_tor_data' '/backup'
