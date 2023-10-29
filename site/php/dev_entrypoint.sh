d48fac276d03ba113fac849e779126acd867692a
JoaoCostaIFG <joaocosta.work@posteo.net>
2023-10-29 21:53:03 +0000

    Trying roadrunner

--- site/php/dev_entrypoint.sh
+++ site/php/dev_entrypoint.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 
-cd /app/main || exit 1
+cd /app/main && \
+  php artisan storage:link && \
+  rm -rf /app/main/storage/app && ln -s /data/main/storage /app/main/storage/app
 
-php artisan storage:link
-
-rm -rf /app/main/storage/app && ln -s /data/main/storage /app/main/storage/app
-
-php artisan octane:status
-
-php artisan octane:start -n --server=roadrunner --host=0.0.0.0 --port=8000
+rr serve -c ./.rr.yaml
