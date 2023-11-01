caebfafa4aa6a1aa2370fd20a8509d36b64f97ea
JoaoCostaIFG <joaocosta.work@posteo.net>
2023-11-01 02:38:08 +0000

    No more wiki

--- site/php/dev_entrypoint.sh
+++ site/php/dev_entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-cd /app/main && \
-  php artisan storage:link && \
-  rm -rf /app/main/storage/app && ln -s /data/main/storage /app/main/storage/app
+php artisan storage:link
+
+rm -rf /app/storage/app
+ln -s /data/storage /app/storage/app
 
 rr serve -c ./.rr.yaml
