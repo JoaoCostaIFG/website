23c6cbe3051eaef0824dd8eb45ce9b07edc54805
JoaoCostaIFG <joaocosta.work@posteo.net>
2024-07-31 02:11:43 +0100

    Clean website development files

--- site/dev.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-echo "IMPORTANT: Don't forget to run 'npm run dev'"
-echo "IMPORTANT: The address if http://localhost:8000"
-
-docker compose -f docker-compose.yml -f development.yml up --build
--- site/update_php_deps.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-docker exec -it php composer update
