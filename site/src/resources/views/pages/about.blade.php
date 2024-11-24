7d7cca4a3da18d4237fe493e65188dbc22ea2299
JoaoCostaIFG <joaocosta.work@posteo.net>
2024-11-24 23:32:19 +0000

    feat: update 'about me' to reflect me better

--- site/src/resources/views/pages/about.blade.php
+++ site/src/resources/views/pages/about.blade.php
@@ -6,30 +6,44 @@
   <div class="prose grid grid-cols-12 gap-4">
     <div class="col-span-12 sm:col-span-8">
       <p>
-        I'm a twenty-something year old computer/informatics engineer. I finished by master's degree
-        at <a href="https://fe.up.pt">Faculty of Engineering of the University of Porto</a> in 2023. I started this website
-        so I could learn more about web development and have
-        a place to share my experiences/thoughts (mostly) relating to informatics. This
-        website passed through multiple changes and is now hosted on my personal home-server
-        (alongside other self-hosted services I use).
+        I'm a twenty-something year old software engineer. I finished by master's degree
+        at <a href="https://fe.up.pt">Faculty of Engineering of the University of Porto</a> in 2023.
+        This website started as a place where I could learn more about web development. Now, it
+        is somewhere where I try to share a bit of my experiences and thoughts (mostly) relating
+        to informatics. There have been multiple iterations of the website over the years, so
+        don't get too surprised if you come here one day and everything looks different. The
+        website is now hosted on my personal home-server (alongside other self-hosted services I use).
       </p>
       <p>
-        As part of both my academic, work, and personal experience, I've had the
-        opportunity to deal with numerous programming languages. As of writing, my
-        favorites are: <b>Bash/POSIX shell</b>, <b>C</b>, <b>C++</b>, <b>Java</b>,
-        <b>Python3</b>, <b>Typescript</b>, <b>PHP</b> and <b>Zig</b>.
+        I usually write with little to no spell-checking tools, so I'm sure there are lots
+        of spelling and grammar errors all over. My partner (who is probably my only reader)
+        ends up pointing out most of the errors, so I can fix them. Still, feel free to shoot
+        me an email if you thing something not quite right.
+      </p>
+      <p>
+        Professionally, my main foxus has been on several parts of software development for
+        embedded systems. I've started by working on tests for DO-178C (an aviation standard)
+        certification of an OS (Operating System). Then, I've moved one to tooling,
+        packaging, and releases: a sort of internal consulting at the companies where
+        I worked. Now, I'm working as a software developer for Linux and baremetal drivers.
       </p>
       <p>
         In my free time, I enjoy working on personal projects, listening to music,
         playing games, and reading. Many of my personal projects revolve around
         customizing my Linux machines, be it the personal ones or the servers
-        (<em>I use Arch BTW</em>).
+        (<em>I use Arch BTW</em>), or automating stuff.
+      </p>
+      <p>
+        In my work life, I've programmed in <b>Bash/POSIX shell</b>, <b>C</b>, <b>C++</b>,
+        <b>JavaScript</b>, <b>Python</b>, and <b>Scala</b>. In my personal projects I mostly
+        use <b>Bash/POSIX shell</b>, <b>JavaScript</b>, <b>Python</b>, and <b>PHP</b>. Now
+        I'm trying to get more proficient in <b>Rust</b>.
       </p>
       <p>
         I enjoy contributing to open-source software and always try to release mine
         with open licenses (often the MIT license). Generally speaking, I don't like
-        social media, so I try to stay as far away from it as possible. This isn't
-        always possible/feasible (e.g. LinkedIn).
+        social media, so I try to stay as far away from it as possible. Still, I have
+        a LinkedIn account for work reasons.
       </p>
       <p>
         The best/fastest way to contact me is through email at:
