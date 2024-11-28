33ed471cfa22e6a9d382253ca7dfac163633a310
JoaoCostaIFG <joaocosta.work@posteo.net>
2024-11-28 19:38:49 +0000

    Fix: move cache generation to post build

--- site/src/prod_entrypoint.sh
+++ site/src/prod_entrypoint.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+php artisan ziggy:generate
+
+php artisan storage:link
+rm -rf storage/app
+ln -s /data/storage storage/app
+
 source "$ENV_PASSWORD_FILE"
 php artisan env:decrypt --env=production
 
