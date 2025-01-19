302dc6775beda800ffb5d8a21196fb49d4656621
João Costa <joaocosta.work@posteo.net>
2025-01-19 15:27:13 +0000

    Update icon.blade.php

--- site/src/resources/views/partials/nav/icon.blade.php
+++ site/src/resources/views/partials/nav/icon.blade.php
@@ -1,4 +1,4 @@
 <a class="text-xl w-5 text-navbar-400 hover:text-white <?php if (isset($classes)) echo $classes ?>"
-    title="{{ $title }}" href="{{ $href }}">
+    title="{{ $title }}" href="{{ $href }}" rel="{{ $rel }}">
   <i class="{{ $icon }}"></i>
 </a>
