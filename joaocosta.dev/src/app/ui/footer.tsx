9b31b42a7cc64a719debfc8f937cf11f2385e15e
JoaoCostaIFG <joaocosta.work@posteo.net>
2025-03-13 22:23:34 +0000

    feat: Implement syntax highlighting for blog posts
    Adds `react-syntax-highlighter` and `parse-numeric-range` to
    support syntax highlighting in blog posts.
    
    - Integrates `react-syntax-highlighter` for code rendering.
    - Uses `parse-numeric-range` to highlight specific lines in code.
    - Updates blog post rendering to use the new component.
    
    Signed-off-by: JoaoCostaIFG <joaocosta.work@posteo.net>

--- joaocosta.dev/src/app/ui/footer.tsx
+++ joaocosta.dev/src/app/ui/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="container sticky top-[100vh] mb-4 p-0">
-      <hr className="border-1 border-foreground-600 dark:border-foreground-500 my-4 w-full" />
+      <hr className="border-1 border-gray-500 my-4 w-full" />
       <p className="muted text-sm">
         Copyright <a className="anchor" rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons</a>
         <i className="fa-solid fa-copyright"></i> <b>João Costa</b>
