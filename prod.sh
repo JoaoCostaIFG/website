b758bae72b6ec28c6dee1a47f2afe851ee72c7c7
JoaoCostaIFG <joaocosta.work@posteo.net>
2025-05-24 21:31:26 +0100

    wip: merging immich compose definitions
    Signed-off-by: JoaoCostaIFG <joaocosta.work@posteo.net>

--- prod.sh
+++ prod.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-docker compose -f production.yml "$@"
-docker compose -f immich.yml --env-file /usr/local/etc/immich.env "$@"
+docker compose -f production.yml -f immich.yml --env-file /usr/local/etc/immich.env "$@"
